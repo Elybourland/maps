cf10e470a0479638fb9ddcd815d9501d9760c46d
Elybourland <JEBourland@yahoo.com>
2022-01-01 10:57:52 -0800

    setting up google maps

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -136939,7 +136939,7 @@ function () {
 exports.Company = Company;
 ;
 },{"faker":"node_modules/faker/index.js"}],"src/index.ts":[function(require,module,exports) {
-"use strict";
+"use strict"; /// <reference types="@types/google.maps" />
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -136953,6 +136953,7 @@ var user = new User_1.User();
 console.log(user);
 var company = new Company_1.Company();
 console.log(company);
+google;
 },{"./User":"src/User.ts","./Company":"src/Company.ts"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from './User';
 import { Company } from './Company';
 
@@ -8,3 +10,5 @@ console.log(user);
 const company = new Company();
 
 console.log(company);
+
+google
